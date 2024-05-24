3370486a934846181d47382d62ff4a371f1c49a2
Tom Schwartz <thomas@languapps.com>
2024-05-24 14:03:38 +0200

    new

--- src/components/pages/A1/NounsPage.js
+++ src/components/pages/A1/NounsPage.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import GrammarTopic from '../../GrammarTopic';
 import NounsData from '../../../data/grammar/A1/1Nouns.json';
-import PageTurner from '../../common/PageTurner'; // Import PageTurner component
+import PageTurner from '../../common/PageTurner';
 
 const NounsPage = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const { pages } = NounsData;
+
+  useEffect(() => {
+    console.log(`Current Page: ${currentPage}`);
+    console.log(`Current Questions:`, pages[currentPage]?.questions);
+  }, [currentPage]);
 
   if (!pages || pages.length === 0) {
     return <div>No content available.</div>;
   }
 
   const currentQuestions = pages[currentPage]?.questions || [];
-  console.log('Current Questions:', currentQuestions);
 
   const nextPage = () => {
     setCurrentPage((prevPage) => Math.min(prevPage + 1, pages.length - 1));
@@ -26,7 +30,6 @@ const NounsPage = () => {
     <div>
       <h1>Nouns</h1>
       <GrammarTopic contentData={currentQuestions} />
-
       <PageTurner
         currentPage={currentPage + 1}
         totalPages={pages.length}
