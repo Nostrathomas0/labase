1127aecb104c337772fd3dae9446cab8d2047ad0
Tom Schwartz <thomas@languapps.com>
2024-07-10 22:17:52 +0200

    auth

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,12 +1,33 @@
-// src/ProtectedRoute.js
+// src/components/ProtectedRoute.js
 import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from './AuthContext';
+import { Route, Redirect } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 
-const ProtectedRoute = () => {
-  const { currentUser} = useAuth();
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const isAuthenticated = () => {
+    const token = document.cookie.split(';').find(cookie => cookie.trim().startsWith('authToken='));
+    if (!token) return false;
 
-  return currentUser ? <Outlet /> : <Navigate to="/" />;
+    try {
+      const decodedToken = jwtDecode(token.split('=')[1]);
+      return decodedToken && decodedToken.exp > Date.now() / 1000;
+    } catch (error) {
+      return false;
+    }
+  };
+
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        isAuthenticated() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
 };
 
 export default ProtectedRoute;
