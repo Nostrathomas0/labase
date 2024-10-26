a4ef279f8034ba2a8604f66efc02f09b292a2f13
Tom Schwartz <thomas@languapps.com>
2024-10-27 01:15:11 +0200

    d

--- src/components/common/PageTurner.js
+++ src/components/common/PageTurner.js
@@ -1,12 +1,25 @@
-// PageTurner.js
 import React from 'react';
 
 const PageTurner = ({ currentPage, totalPages, onNext, onPrevious }) => {
   return (
-    <div>
-      <p>Page {currentPage} of {totalPages}</p>
-      <button onClick={onPrevious} disabled={currentPage <= 1}>Previous</button>
-      <button onClick={onNext} disabled={currentPage >= totalPages}>Next</button>
+    <div className="page-turner-container">
+      <p className="page-info">Page {currentPage} of {totalPages}</p>
+      <div className="page-buttons">
+        <button
+          className="prev-button"
+          onClick={onPrevious}
+          disabled={currentPage <= 1}
+        >
+          Previous
+        </button>
+        <button
+          className="next-button"
+          onClick={onNext}
+          disabled={currentPage >= totalPages}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
