b22cbb63a9521a3e8e97eda79267d0eec8433d8e
Tom Schwartz <thomas@languapps.com>
2024-11-01 10:13:53 +0100

    d

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route, Link, useNavigate, useLocation } from 'react-router-dom';
+import { Routes, Route, Link, useLocation } from 'react-router-dom';
 import { AuthProvider } from './AuthContext';
 import Cookies from 'js-cookie';
-import { jwtDecode } from 'jwt-decode';
+import jwtDecode from 'jwt-decode';
 
-import CoverModal from './components/CoverModal'; // Adjust the import path as necessary
+import CoverModal from './components/CoverModal'; 
 import A1 from './components/pages/A1';
 import A2 from './components/pages/A2';
 import B1 from './components/pages/B1';
@@ -37,14 +37,14 @@ import FuturePerfPage from './components/pages/B2/FuturePerfPage';
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [authError, setAuthError] = useState('');
-  const navigate = useNavigate();
   const location = useLocation();
 
   useEffect(() => {
     const backendJwtToken = Cookies.get('backendJwtToken');  // Check for the correct cookie
+    console.log("Retrieved backendJwtToken from cookies:", backendJwtToken); // Debugging
+
     if (!backendJwtToken) {
-      setAuthError('No authentication token found. Please sign in.');
-      navigate('/login');  // Redirect if no token
+      setAuthError('No authentication token found. Please sign in at ');
       return;
     }
 
@@ -52,14 +52,12 @@ function App() {
       const decodedToken = jwtDecode(backendJwtToken);
       if (decodedToken.exp < Date.now() / 1000) {
         setAuthError('Session expired. Please sign in again.');
-        navigate('/login');  // Redirect if token expired
       }
     } catch (error) {
       console.error("Invalid token:", error);
-      setAuthError('Invalid authentication. Please sign in.');
-      navigate('/login');
+      setAuthError('Invalid authentication. Please sign in at ');
     }
-  }, [navigate]);
+  }, []);
 
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
@@ -75,60 +73,62 @@ function App() {
   return (
     <AuthProvider>
       <div>
-        {authError && (
+        {authError ? (
           <div className="auth-error">
-          {authError} 
-          <a href="https://languapps.com">languapps.com</a>
-        </div>
-        )}
-        <div className="levels-topics-wrapper">
-          <div className="levels-and-topics-container">
-            <div className="levels-container">
-              <h2>Levels</h2>
-              <nav>
-                <ul className="menu-list">
-                  <li><Link to="/A1">Beginner (A1)</Link></li>
-                  <li><Link to="/A2">Elementary (A2)</Link></li>
-                  <li><Link to="/B1">Pre-Intermediate (B1)</Link></li>
-                  <li><Link to="/B2">Intermediate (B2)</Link></li>
-                </ul>
-              </nav>
-            </div>
-            <div className="topics-container">
-              <h2>Level Topics</h2>
-              <div className="topics-list"></div>
-            </div>
+            {authError}
+            <a href="https://languapps.com">languapps.com</a>
           </div>
-        </div>
+        ) : (
+          <>
+            <div className="levels-topics-wrapper">
+              <div className="levels-and-topics-container">
+                <div className="levels-container">
+                  <h2>Levels</h2>
+                  <nav>
+                    <ul className="menu-list">
+                      <li><Link to="/A1">Beginner (A1)</Link></li>
+                      <li><Link to="/A2">Elementary (A2)</Link></li>
+                      <li><Link to="/B1">Pre-Intermediate (B1)</Link></li>
+                      <li><Link to="/B2">Intermediate (B2)</Link></li>
+                    </ul>
+                  </nav>
+                </div>
+                <div className="topics-container">
+                  <h2>Level Topics</h2>
+                  <div className="topics-list"></div>
+                </div>
+              </div>
+            </div>
 
-        <Routes>
-          <Route path="/A1" element={<A1 />} />
-          <Route path="/A2" element={<A2 />} />
-          <Route path="/B1" element={<B1 />} />
-          <Route path="/B2" element={<B2 />} />
-          <Route path="/A1/nouns" element={<NounsPage />} />
-          <Route path="/A1/adjectives" element={<AdjectivesPage />} />
-          <Route path="/A1/verbs" element={<VerbsPage />} />
-          <Route path="/A1/there" element={<TherePage />} />
-          <Route path="/A2/pastCont" element={<PastContPage />} />
-          <Route path="/A2/future" element={<FuturePage />} />
-          <Route path="/A2/goingTo" element={<GoingToPage />} />
-          <Route path="/A2/compSupe" element={<CompSupePage />} />
-          <Route path="/B1/presPerfCont" element={<PresPerfContPage />} />
-          <Route path="/B1/pastPerfCont" element={<PastPerfContPage />} />
-          <Route path="/B1/2ndCond" element={<SecCondPage />} />
-          <Route path="/B1/modalVerbs" element={<ModalVerbsPage />} />
-          <Route path="/B2/mixedCond" element={<MixedCondPage />} />
-          <Route path="/B2/causitives" element={<CausitivesPage />} />
-          <Route path="/B2/modalsProb" element={<ModalsProbPage />} />
-          <Route path="/B2/futurePerf" element={<FuturePerfPage />} />
-        </Routes>
+            <Routes>
+              <Route path="/A1" element={<A1 />} />
+              <Route path="/A2" element={<A2 />} />
+              <Route path="/B1" element={<B1 />} />
+              <Route path="/B2" element={<B2 />} />
+              <Route path="/A1/nouns" element={<NounsPage />} />
+              <Route path="/A1/adjectives" element={<AdjectivesPage />} />
+              <Route path="/A1/verbs" element={<VerbsPage />} />
+              <Route path="/A1/there" element={<TherePage />} />
+              <Route path="/A2/pastCont" element={<PastContPage />} />
+              <Route path="/A2/future" element={<FuturePage />} />
+              <Route path="/A2/goingTo" element={<GoingToPage />} />
+              <Route path="/A2/compSupe" element={<CompSupePage />} />
+              <Route path="/B1/presPerfCont" element={<PresPerfContPage />} />
+              <Route path="/B1/pastPerfCont" element={<PastPerfContPage />} />
+              <Route path="/B1/2ndCond" element={<SecCondPage />} />
+              <Route path="/B1/modalVerbs" element={<ModalVerbsPage />} />
+              <Route path="/B2/mixedCond" element={<MixedCondPage />} />
+              <Route path="/B2/causitives" element={<CausitivesPage />} />
+              <Route path="/B2/modalsProb" element={<ModalsProbPage />} />
+              <Route path="/B2/futurePerf" element={<FuturePerfPage />} />
+            </Routes>
 
-        <CoverModal isOpen={isModalOpen} onRequestClose={closeModal} />
+            <CoverModal isOpen={isModalOpen} onRequestClose={closeModal} />
+          </>
+        )}
       </div>
     </AuthProvider>
   );
 }
 
 export default App;
-
