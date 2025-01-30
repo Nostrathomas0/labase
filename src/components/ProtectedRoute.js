7b15957783ff649ad1de564fce74f43ca6eb5d38
Tom Schwartz <thomas@languapps.com>
2025-01-30 14:50:52 +0100

    d

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,4 +1,5 @@
 // assets/js/ProtectedRoute.js
+import { auth } from '../firebaseInit';
 import React, { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
