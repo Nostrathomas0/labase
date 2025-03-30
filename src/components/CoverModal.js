755eb866674f7d637ae5613b299a231ad511e9e0
Tom Schwartz <thomas@languapps.com>
2025-03-30 20:46:58 +0200

    s

--- src/components/CoverModal.js
+++ src/components/CoverModal.js
@@ -1,35 +1,36 @@
 // CoverModal.js
-import React from 'react';
+// import React from 'react';
 import { useAuth } from './AuthContext';
 
 const CoverModal = ({ isOpen, onClose }) => {
-  const { userEmail, currentUser } = useAuth();
+//  const { userEmail, currentUser } = useAuth();
   
-  return (
-    <div className={`modal-overlay ${isOpen ? 'visable' : 'hidden'}`}>
-      <div className="modal-content">
-        <button className="close-button" onClick={onClose}>×</button>
-        
-        <div className="modal-header">
-          {currentUser && userEmail ? (
-            <h2>Welcome back, {userEmail}!</h2>
-          ) : (
-            <h2>to to Languapps</h2>
-          )}
-        </div>
-        
-        <div className="modal-body">
-          {/* Modal content */}
-          <p>A reactive Web App for English learners</p>
-        </div>
-        
-        <div className="modal-footer">
-          <button onClick={onClose}>Close</button>
-          {/* Other buttons */}
-        </div>
-      </div>
-    </div>
-  );
+  return null
+//(
+//  <div className={`modal-overlay ${isOpen ? 'visable' : 'hidden'}`}>
+//    <div className="modal-content">
+//      <button className="close-button" onClick={onClose}>×</button>
+//      
+//      <div className="modal-header">
+//        {currentUser && userEmail ? (
+//          <h2>Welcome back, {userEmail}!</h2>
+//        ) : (
+//          <h2>to to Languapps</h2>
+//        )}
+//      </div>
+//      
+//      <div className="modal-body">
+//        {/* Modal content */}
+//        <p>A reactive Web App for English learners</p>
+//      </div>
+//      
+//      <div className="modal-footer">
+//        <button onClick={onClose}>Close</button>
+//        {/* Other buttons */}
+//      </div>
+//    </div>
+//  </div>
+//);
 };
 
 export default CoverModal;
